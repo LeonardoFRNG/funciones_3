2d621328bcce1230f987b6993b7a4c18c5601860
LeonardoFRNG <leonardojim321@gmail.com>
2026-03-16 15:24:39 -0500

    a

--- ejercicio.py
+++ ejercicio.py
@@ -1,3 +1,24 @@
+# 🟢 Ejercicio 1 — Fácil
+# Determinar si un número es positivo, negativo o cero
+
+# Crea una función llamada:analizar_numero(numero)
+
+# La función debe:Recibir un número
+
+# Determinar si el número es:
+
+# positivo
+# negativo
+# cero
+
+# Retornar un mensaje con el resultado.
+# El programa principal debe:
+# Pedir al usuario que ingrese un número.
+
+# Llamar a la función analizar_numero.
+
+# Mostrar el resultado con print.
+
 def analizar_numero(numero):
 
     if numero > 0:
--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,25 @@
+# Función para determinar si una persona puede votar
+
+# Crea una función llamada:puede_votar(edad)
+
+# La función debe:
+
+# Recibir la edad de una persona
+
+# Si la edad es mayor o igual a 18, retornar "Puede votar"
+
+# Si no, retornar "No puede votar"
+
+# El programa principal debe:
+
+# a. Pedir el nombre
+
+# b. Pedir la edad
+
+# c. Llamar a la función
+
+# d. Mostrar el resultado en pantalla.
+
 def puede_votar(edad):
     if edad >= 18:
         return "Puede Votar"
@@ -10,4 +32,4 @@ edad = int(input("Ingrese su edad: "))
 
 
 print(f"{nombre} usted tiene: {edad} y {puede_votar(edad)}")
-s
+
